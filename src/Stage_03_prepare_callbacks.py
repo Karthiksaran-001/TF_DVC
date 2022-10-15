5548413caffdfba3f79ab48239f769b8b283b167
Karthiksaran-001 <mailmekarthik001@gmail.com>
2022-10-15 13:13:47 +0530

    Stage-03 is Finished

--- src/Stage_03_prepare_callbacks.py
+++ src/Stage_03_prepare_callbacks.py
@@ -1,10 +1,8 @@
 from ast import arg
 from src.utils.all_utils import read_yaml   , create_directory
 import argparse
-import pandas as pd
 import os
-from tqdm import tqdm
-import shutil ## Help to copy files
+from src.utils.callbacks import create_save_tensorboard_callbacks, create_save_checkpoint_callbacks
 import logging
 
 logging.basicConfig(
@@ -14,10 +12,24 @@ logging.basicConfig(
     filemode="a",
 )
 
+
 def get_callbacks(config_path , params_path):
    config = read_yaml(config_path)
    params = read_yaml(params_path)
+
+   artifacts = config["artifacts"]
+   artifacts_dir = artifacts["artifacts_dir"]
    
+   tensorboard_logdir = os.path.join(artifacts_dir , artifacts["TENSORBOARD_ROOT_LOG_DIR"])
+   checkpoint_dir = os.path.join(artifacts_dir , artifacts["Checkpoint_Dir"])
+   callback_dir = os.path.join(artifacts_dir , artifacts["CALLBACKS_DIR"])
+
+   create_directory([tensorboard_logdir,checkpoint_dir,callback_dir])
+   create_save_tensorboard_callbacks(callback_dir , tensorboard_logdir)
+   create_save_checkpoint_callbacks(callback_dir , tensorboard_logdir)
+   
+
+
 
 
 if __name__ == "__main__":
